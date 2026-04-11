e00044669cdb22fcda5fbee6e9f54fb1c0b9d8b2
tigergalaxy1st <dengtisi130@aliyun.com>
2026-04-11 15:52:27 +0800

    trigger

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,4 @@
-// trigger
-
-pluginManagement {
-    repositories { google(); mavenCentral(); gradlePluginPortal() }
-}
+pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
