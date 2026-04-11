a323574159ad9708d0605ff22a095f695be1b3ac
tigergalaxy1st <dengtisi130@aliyun.com>
2026-04-11 13:04:25 +0800

    fix: add JitPack repo for Xposed API (via rovo89/XposedBridge)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,24 @@
-// Trigger CI build
-
 pluginManagement {
     repositories {
-        google() 
-        mavenCentral() 
-        gradlePluginPortal() 
+        google()
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS) 
-    repositories { 
-        google() 
-        mavenCentral() 
-        mavenLocal() 
-    } 
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            name = "JitPack"
+            url = uri("https://jitpack.io")
+            content {
+                includeGroup("com.github.rovo89")
+            }
+        }
+    }
 }
 
 rootProject.name = "MiTrustBuster"
