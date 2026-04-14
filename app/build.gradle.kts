472755d4c0ed99ed17b05223c3d5e74062ab98e4
tigergalaxy1st <dengtisi130@aliyun.com>
2026-04-14 09:18:35 +0800

    fix: add Material Components library dependency to resolve theme color attrs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 }
 
 dependencies {
+    implementation("com.google.android.material:material:1.11.0")
     compileOnly("de.robv.android.xposed:api:82")
     compileOnly("de.robv.android.xposed:api:82:sources")
     implementation("org.luckypray:dexkit:2.0.2")
